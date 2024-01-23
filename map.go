7605e01312bb2a5547a35bd1a277ec2db7c50156
Tyler <tyler@auroradev.org>
2024-01-22 21:28:13 -0500

    [feature] Add JSON map support
    - Adds JSON map file support

--- map.go
+++ map.go
@@ -2,10 +2,18 @@ package redirector
 
 import (
 	"encoding/csv"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 	"io"
 	"os"
+	"path"
 	"strings"
 )
+
+var ErrUnsupportedFormat = errors.New("unsupported map format")
 
 // loadMapFile loads a file as a map
 func loadMapFile(file string) (map[string]string, error) {
@@ -17,11 +25,20 @@ func loadMapFile(file string) (map[string]string, error) {
 
 	defer f.Close()
 
-	return loadMap(f)
+	ext := path.Ext(file)
+
+	switch ext {
+	case ".csv":
+		return loadMapCSV(f)
+	case ".json":
+		return loadMapJSON(f)
+	}
+
+	return nil, ErrUnsupportedFormat
 }
 
-// loadMap loads a pipe separated file of mappings
-func loadMap(f io.Reader) (map[string]string, error) {
+// loadMapCSV loads a pipe separated file of mappings
+func loadMapCSV(f io.Reader) (map[string]string, error) {
 	m := make(map[string]string)
 
 	r := csv.NewReader(f)
@@ -40,6 +57,47 @@ func loadMap(f io.Reader) (map[string]string, error) {
 		}
 
 		m[strings.TrimLeft(row[0], "/")] = strings.TrimLeft(row[1], "/")
+	}
+
+	return m, nil
+}
+
+// ReleaseFile represents a file to be mapped
+type ReleaseFile struct {
+	BoardSlug     string `json:"board_slug"`
+	FileURI       string `json:"file_uri"`
+	FileUpdated   string `json:"file_updated"`
+	FileSize      string `json:"file_size"`
+	DistroRelease string `json:"distro_release"`
+	KernelBranch  string `json:"kernel_branch"`
+	ImageVariant  string `json:"image_variant"`
+	Preinstalled  string `json:"preinstalled_application"`
+	Promoted      string `json:"promoted"`
+	Repository    string `json:"download_repository"`
+	Extension     string `json:"file_extension"`
+}
+
+var distroCaser = cases.Title(language.Und)
+
+// loadMapJSON loads a map file from JSON, based on the format specified in the github issue.
+// See: https://github.com/armbian/os/pull/129
+func loadMapJSON(f io.Reader) (map[string]string, error) {
+	m := make(map[string]string)
+
+	var files []ReleaseFile
+
+	if err := json.NewDecoder(f).Decode(&files); err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		builtUri := fmt.Sprintf("%s/%s-%s.%s",
+			file.BoardSlug,
+			distroCaser.String(file.DistroRelease),
+			file.KernelBranch,
+			file.Extension)
+
+		m[builtUri] = file.FileURI
 	}
 
 	return m, nil
--- map_test.go
+++ map_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 var _ = Describe("Map", func() {
-	It("Should successfully load the map", func() {
-		m, err := loadMap(strings.NewReader(`bananapi/Bullseye_current|bananapi/archive/Armbian_21.08.1_Bananapi_bullseye_current_5.10.60.img.xz|Aug 26 2021|332M`))
+	It("Should successfully load the map from a CSV/Pipe separated file", func() {
+		m, err := loadMapCSV(strings.NewReader(`bananapi/Bullseye_current|bananapi/archive/Armbian_21.08.1_Bananapi_bullseye_current_5.10.60.img.xz|Aug 26 2021|332M`))
 
 		Expect(err).To(BeNil())
 		Expect(m["bananapi/Bullseye_current"]).To(Equal("bananapi/archive/Armbian_21.08.1_Bananapi_bullseye_current_5.10.60.img.xz"))
